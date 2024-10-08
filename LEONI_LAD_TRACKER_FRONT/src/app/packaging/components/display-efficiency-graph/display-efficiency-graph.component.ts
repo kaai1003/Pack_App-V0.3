461552b341c858a23315a2e900e339f4ddda74d2
aimad kacem <kacem.aimad@gmail.com>
2024-10-08 10:18:07 +0100

    Improved Efficiency Graph code

--- LEONI_LAD_TRACKER_FRONT/src/app/packaging/components/display-efficiency-graph/display-efficiency-graph.component.ts
+++ LEONI_LAD_TRACKER_FRONT/src/app/packaging/components/display-efficiency-graph/display-efficiency-graph.component.ts
@@ -19,20 +19,32 @@ export class DisplayEfficiencyGraphComponent implements OnInit {
   ngOnInit(): void {
     const ctx = document.getElementById('EfficiencyGraph') as HTMLCanvasElement;
     const chart = new Chart(ctx, {
-      type: 'line',
       data: {
         labels: ['Efficiency Per Hour'], // Provide your labels
         datasets: [
           {
+            type: 'line',
+            label: 'Efficiency',
             data: [], // Provide your data
             borderWidth: 6,
             fill: false,
             borderColor: 'oklch(0.28 0.1 255.67)',
             tension: 0.1,
           },
+          // {
+          //   type: 'line',
+          //   label: 'Target',
+          //   data: [],
+          //   fill: false,
+          //   fillColor: 'red',
+          //   borderColor: 'green',
+          //   tension: 0.1,
+          // },
         ],
       },
       options: {
+        animation: { duration: 0 },
+        scales: { y: { min: 0, max: 150 } },
         responsive: true,
         maintainAspectRatio: true,
         plugins: {
@@ -63,11 +75,8 @@ export class DisplayEfficiencyGraphComponent implements OnInit {
           chart.data.labels = data.map(
             (item) => item.hour + 'h -> ' + (item.hour + 1) + 'h'
           );
-          chart.data.datasets[0].data = [
-            ...data.map((item) => item.efficiency),
-            100,
-          ];
-          chart.data.datasets[0].label = 'Efficiency';
+          chart.data.datasets[0].data = data.map((item) => item.efficiency);
+          //chart.data.datasets[1].data = data.map((item) => 100);
           chart.update();
         });
     };
